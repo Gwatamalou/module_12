a2df902c848903d1527fe8a9d2c3d514b2f01658
Gwatamalou <96253655+Gwatamalou@users.noreply.github.com>
2024-08-04 21:54:02 +0600

    Update runner_test.py

--- tests_12_1/runner_test.py
+++ tests_12_1/runner_test.py
@@ -1,21 +1,25 @@
-import runner as r
+from tests_12_1 import runner as r
 import unittest
 
 
 class RunnerTest(unittest.TestCase):
 
+    is_frozen = False
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         r1 = r.Runner('name1')
         for _ in range(10):
             r1.walk()
         self.assertEqual(r1.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         r2 = r.Runner('name2')
         for _ in range(10):
             r2.run()
         self.assertEqual(r2.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         r3 = r.Runner('name1')
         r4 = r.Runner('name2')
@@ -27,5 +31,4 @@ class RunnerTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-
     unittest.main()
